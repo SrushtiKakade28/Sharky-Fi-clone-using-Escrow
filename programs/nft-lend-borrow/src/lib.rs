269b52313f8b7f85b5af642b7c5c7345ea04bdcc
SrushtiKakade28 <srushtikakade28@gmail.com>
2023-12-24 22:54:36 -0800

    demo commit

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("8CFkU9WdLz92brmmkqC5SXDxU78wK1XiEsgHJ2iSfXLX");
 
 #[program]
 pub mod nft_lend_borrow {
